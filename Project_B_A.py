72863cb1fe0cb368eb13447d7618cf36ee946848
Keller Sedillo <kellersedillo@hotmail.com>
2022-11-20 15:59:33 -0700

    Update Project_B_A.py and add screenshot

--- Project_B_A.py
+++ Project_B_A.py
@@ -4,18 +4,16 @@
 # CS488  
 # Final Project Basic Analysis
 # 10/13/22
+
+#wget https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh && mkdir ./.conda && bash Miniconda3-latest-Linux-x86_64.sh -b && rm -f Miniconda3-latest-Linux-x86_64.sh 
+
+
 import os
 import math
 import pandas as pd 
 import numpy as np
 import statistics as stat
 from matplotlib import pyplot as plt
-from sklearn.cluster import KMeans
-from sklearn.decomposition import PCA
-from sklearn.datasets import load_digits
-from sklearn.metrics import classification_report, accuracy_score
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
 from sklearn.datasets import load_digits
@@ -28,13 +26,6 @@ from sklearn.mixture import GaussianMixture
 from sklearn.preprocessing import StandardScaler, normalize
 from sklearn.metrics import silhouette_samples, silhouette_score
 from sklearn import metrics
-from sklearn.datasets import make_classification
-from sklearn.model_selection import KFold
-from sklearn.svm import SVC
-from sklearn.model_selection import cross_val_score
-from sklearn.linear_model import LinearRegression
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import accuracy_score
 
 from six import StringIO
 
@@ -48,11 +39,14 @@ import pydotplus
 # import csv files
 Maths = pd.read_csv("maths.csv")
 Portuguese = pd.read_csv("Portuguese.csv")
+
 # settings options to view the entire data set in terminal
 pd.set_option('display.max_columns', None)
 pd.set_option('display.width', None)
 pd.set_option('display.max_colwidth', None)
+
 ###############   Mean values
+
 # mean values for all entries int Maths based on 'school'
 print('Mean values according to school:')
 print('Math:')
@@ -61,6 +55,7 @@ print()
 print('Portuguese:')
 print(Portuguese.groupby('school').mean())
 print()
+
 # mean values for all entries in Maths based on 'sex'
 print('Mean values according to sex:')
 print('Math:')
@@ -69,6 +64,7 @@ print()
 print('Portuguese:')
 print(Portuguese.groupby('sex').mean())
 print()
+
 # mean values for all entries in Maths based on 'absences'
 print('Mean values according to absences')
 print('Math:')
@@ -77,6 +73,7 @@ print()
 print('Portuguese:')
 print(Portuguese.groupby('absences').mean())
 print()
+
 # mean values for all entries in Maths based on 'failures'
 print('Mean values according to failures')
 print('Math:')
@@ -85,6 +82,7 @@ print()
 print('Portuguese:')
 print(Portuguese.groupby('failures').mean())
 print()
+
 # mean values for all entries in Maths based on 'study time'
 print('Mean values according to study time')
 print('Math:')
@@ -93,6 +91,7 @@ print()
 print('Portuguese:')
 print(Portuguese.groupby('studytime').mean())
 print()
+
 # mean values for all entries in Maths based on 'internet'
 print('Mean values according to internet')
 print('Math:')
@@ -101,6 +100,7 @@ print()
 print('Portuguese:')
 print(Portuguese.groupby('internet').mean())
 print()
+
 # mean values for all entries in Maths based on 'Medu'
 print('Mean values according to Mothers education')
 print('Math:')
@@ -109,6 +109,7 @@ print()
 print('Portuguese:')
 print(Portuguese.groupby('Medu').mean())
 print()
+
 # mean values for all entries in Maths based on 'Fedu'
 print('Mean values according to fathers education')
 print('Math:')
@@ -117,89 +118,22 @@ print()
 print('Portuguese:')
 print(Portuguese.groupby('Fedu').mean())
 print()
+
 # plots to show correlation between differnt term scores and the final grades
 Maths.plot(x = 'G1', y = 'G3', kind = 'scatter', label = 'G1 scores compared to G3 scores')
 plt.show()
+
 Maths.plot(x = 'G1', y = 'G2', kind = 'scatter', label = 'G1 scores compared to G2 scores')
 plt.show()
+
 Maths.plot(x = 'G2', y = 'G3', kind = 'scatter', label = 'G2 scores compared to G3 scores')
 plt.show()
 
-#################################### KNN
-
-cols = [-1]
-
-y = Maths.iloc[-1]
-X = Maths.drop(Maths.columns[cols], axis=1, inplace=True)
-
-X, y = make_classification()
-
-# create training and test sets for kfold and svm 
-x_train, x_test, y_train, y_test = train_test_split(X,y, random_state=1, test_size=0.2, shuffle=True)
-
-E_scores = []
-M_scores = []
-K_vals = [1,3,5,7,9,11]
-
-# Euclidean Distance KNN
-for i in range(12):
-	if i % 2 == 1:
-		N = KNeighborsClassifier(n_neighbors=i)
-		N.fit(x_train, y_train)
-
-		score = N.score(x_test, y_test)
-		#print(score)
-
-		E_scores.append(score)
-
-# Manhattan Distance KNN
-for i in range(12):
-	if i % 2 == 1:
-		N = KNeighborsClassifier(n_neighbors=i, metric='manhattan')
-		N.fit(x_train, y_train)
-
-		score = N.score(x_test, y_test)
-		#print(score)
-
-		M_scores.append(score)
-
-# plot for knn Euclidean Distance
-plt.plot(E_scores, K_vals)
-plt.title('KNN-Euclidean Distance')
-plt.xlabel('Euclidean Scores')
-plt.ylabel('K-Values')
-plt.show()
-
-# plot for knn Manhattan Distance
-plt.plot(M_scores, K_vals)
-plt.title('KNN-Manhattan Distance')
-plt.xlabel('Manhattan Scores')
-plt.ylabel('K-Values')
-plt.show()
 
 
-#################################### K Fold
 
 
-cv = KFold(n_splits=5, random_state=1, shuffle=True)
-# change model to linear regressions
-model = LinearRegression()
-# obtain accuracy scores for the dataset using kfold
-scores = cross_val_score(model, X, y, scoring='neg_mean_absolute_error', cv=cv, n_jobs=-1)
-print('Average Accuracy Score KFold:')
-print(np.mean(np.absolute(scores)))
-print()
 
-
-#################################### SVM 
-
-
-SVM = SVC(kernel='rbf')
-SVM.fit(x_train,y_train)
-predictions = SVM.predict(x_test)
-print('Accuracy Score SVM:')
-print(accuracy_score(y_test, predictions))
-print()
 
 
 print("=================================================")
@@ -207,6 +141,8 @@ print("Apply k-means clustering")
 df = pd.read_csv('Maths.csv',skiprows=1, header=None, usecols=range(1, 33))
 data = df.dropna(axis = 0, how ='any', thresh = None, subset = None, inplace= False)
 data.to_csv('data.csv')
+
+
 #Load Data
 data = load_digits().data
 pca = PCA(2)
@@ -230,9 +166,12 @@ u_labels = np.unique(label)
 for i in u_labels:
     plt.scatter(df[label == i , 0] , df[label == i , 1] , label = i)
 plt.legend()
+
 centers = np.array(kmeans.cluster_centers_)
+
 plt.scatter(centers[:,0], centers[:,1], marker="x", color='black')
 plt.title("train k-means  for best 5 k clusters and black X are the centroid of clusters")
+
 plt.show()
 
 
@@ -260,15 +199,20 @@ X_normalized = normalize(X_scaled)
  
 # Converting the numpy array into a pandas DataFrame
 X_normalized = pd.DataFrame(X_normalized)
+
 # Reducing the dimensions of the data 
 pca = PCA(n_components = 2)
 X_principal = pca.fit_transform(X_normalized)
 X_principal = pd.DataFrame(X_principal)
 X_principal.columns = ['P1', 'P2']
+
+
 #returns the set of X configurations with shorter distance
 def SelBest(arr:list, X:int)->list:
     dx=np.argsort(arr)[:X]
     return arr[dx]
+
+
 n_clusters=np.arange(2, 7)
 sils = []
 sils_err = []
@@ -282,15 +226,21 @@ for n in n_clusters:
         #store  silhouette_score in an array for further calculation
         sil = metrics.silhouette_score(X_principal, labels, metric='euclidean')
         tmp_sil.append(sil)
+
     plt.figure(figsize =(6, 6))
     plt.scatter(X_principal['P1'], X_principal['P2'],c = GaussianMixture(n_components = n).fit_predict(X_principal), cmap ='rainbow', alpha = 0.6) 
     plt.title('Gaussian Mixture Clustering, k = '+str(n))
     plt.show() 
+
     #calculate average of the silhouette_score and its error rates
     val = np.mean(SelBest(np.array(tmp_sil), int(iterations/5)))
     err = np.std(tmp_sil)
     sils.append(val)
     sils_err.append(err) #store score in an array 
+
+
+
+
 # Plotting a bar graph to compare the results
 #Evaluating the different models and Visualizing the results to find best k according to silhouette_score
 plt.bar(n_clusters, sils, yerr = sils_err)
@@ -298,7 +248,6 @@ plt.title("Silhouette Scores", fontsize=20)
 plt.xticks(n_clusters)
 plt.xlabel("N. of clusters")
 plt.ylabel("Score")
-plt.show()
 #plt.show()
 
 
@@ -426,12 +375,8 @@ grade_classifier.fit(X_train, y_train)
 
 
 
-#We can also view how the grade_classifier divide the logic using pydotplus library
-dot_data = StringIO()  
-tree.export_graphviz(grade_classifier, out_file=dot_data, feature_names=student_features)  
-graph = pydotplus.graph_from_dot_data(dot_data.getvalue())  
-Image(graph.create_png()) 
 
+print(y_test)
 
 #let's create our prediction :
 predictions = grade_classifier.predict(X_test)
@@ -446,14 +391,20 @@ print(d)
 #hyperparameters to increase the accuracy score.
 
 
+#We can also view how the grade_classifier divide the logic using pydotplus library
+dot_data = StringIO()  
+tree.export_graphviz(grade_classifier, out_file=dot_data, feature_names=student_features, filled =True, class_names=['0', '1','2'])  
+graph = pydotplus.graph_from_dot_data(dot_data.getvalue())  
+Image(graph.create_png()) 
 
 
+# import pydotplus 
+# from IPython.display import Image
 
-
-
-
-
-
+# dot_data = tree.export_graphviz(clf, feature_names=X.columns, class_names=['mammals','non-mammals'], filled=True, 
+#                                 out_file=None) 
+# graph = pydotplus.graph_from_dot_data(dot_data) 
+# Image(graph.create_png())
 
 
 
